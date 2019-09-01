0f2114276fe7e60370d9e8d9f671225c3d0c2843
RobinRheem <robin.rheem@gmail.com>
2019-09-01 13:54:52 +0900

    Add user app to settings

--- behind/behind/settings.py
+++ behind/behind/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Password validation
--- behind/users/models.py
+++ behind/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLES = (
+        (1, "job_seeker"),
+        (2, "employee")
+    )
+
+    id = models.BigIntegerField(primary_key=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=20,
+        unique=True,
+        help_text='Required. 20 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField(unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
