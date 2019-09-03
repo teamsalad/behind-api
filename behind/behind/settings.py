13f62f30c505c0892c98b18f01094c3eeb260043
RobinRheem <robin.rheem@gmail.com>
2019-09-03 23:06:01 +0900

    Remove oauth 2 provider

--- behind/behind/settings.py
+++ behind/behind/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oauth2_provider',
     'social_django',
     'users.apps.UsersConfig',
 ]
--- behind/behind/urls.py
+++ behind/behind/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
