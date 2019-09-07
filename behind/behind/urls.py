7e6118a0a4be035f6795a805e87bda8485dad7c5
RobinRheem <robin.rheem@gmail.com>
2019-09-07 16:51:04 +0900

    Configured url for auth related features

--- behind/behind/urls.py
+++ behind/behind/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/docs/users/', include('rest_framework.urls')),
+    path('api/v1/users/', include('rest_auth.urls')),
+    path('api/v1/users/registration/', include('rest_auth.registration.urls')),
 ]
-
