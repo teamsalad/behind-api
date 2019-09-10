9a9772845ccd7efcb0538ec23717ee03df6cf6ae
RobinRheem <robin.rheem@gmail.com>
2019-09-10 16:04:10 +0900

    Create company and job models

--- behind/companies/models.py
+++ behind/companies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "jobs"
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    email_domain = models.CharField(max_length=100)
+    jobs = models.ManyToManyField(Job)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "companies"
