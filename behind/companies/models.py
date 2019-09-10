165c19cb49c55780a618195182719cb37494178e
RobinRheem <robin.rheem@gmail.com>
2019-09-10 22:36:08 +0900

    Put out METHOD constant

--- behind/companies/models.py
+++ behind/companies/models.py
@@ -1,4 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+METHODS = (
+    (1, "email"),
+    (2, "business_card"),
+    (3, "proof_of_employment"),
+)
 
 
 class Job(models.Model):
@@ -19,3 +26,32 @@ class Company(models.Model):
 
     class Meta:
         db_table = "companies"
+
+
+class UserJobHistory(models.Model):
+    confirmation_method = models.PositiveSmallIntegerField(
+        choices=METHODS,
+        default=METHODS[0][0]
+    )
+    confirmed = models.BooleanField(default=False)
+    confirmation_information = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='job_histories'
+    )
+    company = models.OneToOneField(
+        Company,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    job = models.OneToOneField(
+        Job,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_job_histories"
