530ff87e445e307a4d8683dd68687ac6669f0d1d
RobinRheem <robin.rheem@gmail.com>
2019-09-15 18:10:25 +0900

    Create questions app models and migrations

--- behind/questions/models.py
+++ behind/questions/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from behind import settings
+from companies.models import Company, Job
+
+
+class Question(models.Model):
+    content = models.TextField()
+    questioner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    answerer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
