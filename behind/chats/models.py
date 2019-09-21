ae70a2f2ffc35c5e4f0597f3a2bcb9daef61a6bc
RobinRheem <robin.rheem@gmail.com>
2019-09-21 16:17:52 +0900

    Implements models for chatting

--- behind/chats/models.py
+++ behind/chats/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+from behind import settings
+from questions.models import Answer
+
+STATUS = (
+    (1, "STARTED"),
+    (2, "STOPPED"),
+)
+
+
+class ChatRoom(models.Model):
+    state = models.PositiveSmallIntegerField(
+        choices=STATUS,
+        default=STATUS[0][0]
+    )
+    answer = models.ForeignKey(
+        Answer,
+        on_delete=models.SET_NULL,
+        related_name='chat_room'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "chat_rooms"
+
+
+class ChatParticipant(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='participating_chat_rooms'
+    )
+    chat_room = models.ForeignKey(
+        ChatRoom,
+        on_delete=models.CASCADE,
+        related_name='participants'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "chat_participants"
+
+
+class ChatMessage(models.Model):
+    message = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    chat_room = models.ForeignKey(
+        ChatRoom,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "chat_messages"
