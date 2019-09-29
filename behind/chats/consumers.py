5c2b4dc154f9df03103ac88a1250865d657c85b7
RobinRheem <robin.rheem@gmail.com>
2019-09-29 14:25:46 +0900

    Change chat consumer to async websocket consumer

--- behind/chats/consumers.py
+++ behind/chats/consumers.py
@@ -1,10 +1,7 @@
-import json
-
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
     """
     ws/ --> Just for websocket stuff.
     path/ --> HTTP stuff.
@@ -17,7 +14,7 @@ class ChatConsumer(WebsocketConsumer):
     `connect` and `disconnect` are websocket protocol layer functions.
     The other functions you define are events
     """
-    def connect(self):
+    async def connect(self):
         """
         Accept connection if user is authorized and
         is a participant of the chat room.
@@ -28,49 +25,47 @@ class ChatConsumer(WebsocketConsumer):
         self.chat_room_group_name = f'chat_room_{self.chat_room_id}'
         # TODO: use code to make error messages for disconnection reasons
         if not self.user.is_authenticated:
-            self.disconnect(1000)
+            await self.disconnect(1000)
         # TODO: Validate if chat room exists and user is participant
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.chat_room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.chat_room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-
+    async def receive(self, content=None, **kwargs):
         # Send message to room group
-        if text_data_json['type'] == 'chat_message':
-            async_to_sync(self.channel_layer.group_send)(
+        if content['type'] == 'chat_message':
+            await self.channel_layer.group_send(
                 self.chat_room_group_name,
                 {
-                    'type': text_data_json['type'],
-                    'message': text_data_json['message']
+                    'type': content['type'],
+                    'message': content['message']
                 }
             )
-        if text_data_json['type'] == 'chat_timer':
-            async_to_sync(self.channel_layer.group_send)(
+        if content['type'] == 'chat_timer':
+            await self.channel_layer.group_send(
                 self.chat_room_group_name,
                 {
-                    'type': text_data_json['type'],
-                    'time_left': text_data_json['time_left']
+                    'type': content['type'],
+                    'time_left': content['time_left']
                 }
             )
 
-    def chat_message(self, event):
-        self.send(text_data=json.dumps({
+    async def chat_message(self, event):
+        await self.send_json({
             'user_id': self.user.id,
             'message': event['message']
-        }))
+        })
 
-    def chat_timer(self, event):
-        self.send(text_data=json.dumps({
+    async def chat_timer(self, event):
+        await self.send_json({
             'user_id': self.user.id,
             'time_left': event['time_left']
-        }))
+        })
