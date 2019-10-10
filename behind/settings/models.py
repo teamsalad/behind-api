1b423f7d98505d39c1e6282664e3658dfa50b2d4
RobinRheem <robin.rheem@gmail.com>
2019-10-11 00:08:48 +0900

    Create push notification setting model

--- behind/settings/models.py
+++ behind/settings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from behind import settings
+
+
+class PushNotificationSetting(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='push_notification_setting'
+    )
+    chat = models.BooleanField(default=True)
+    answered = models.BooleanField(default=True)
+    asked = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "push_notification_settings"
