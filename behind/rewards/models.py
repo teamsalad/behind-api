8203e7e7cd8f9cdae49246155d6e97a893ccf009
RobinRheem <robin.rheem@gmail.com>
2019-10-12 22:14:32 +0900

    Create gifticon model

--- behind/rewards/models.py
+++ behind/rewards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Gifticon(models.Model):
+    name = models.CharField(max_length=100)
+    exchange = models.CharField(max_length=100)
+    barcode_number = models.CharField(max_length=100)
+    expired_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
