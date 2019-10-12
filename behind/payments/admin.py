c10611be897db1aedef083cd961e15eef66cbddb
RobinRheem <robin.rheem@gmail.com>
2019-10-12 20:48:00 +0900

    Add payment model to admin tool

--- behind/payments/admin.py
+++ behind/payments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register(Payment, PaymentAdmin)
