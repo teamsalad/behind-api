5a2fb6dfb58f0aa07537d8dd3ea3284af2a8252c
RobinRheem <robin.rheem@gmail.com>
2019-10-13 12:51:09 +0900

    Add price to gifticon and app version migrations

--- behind/rewards/models.py
+++ behind/rewards/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 
 class Gifticon(models.Model):
     name = models.CharField(max_length=100)
     exchange = models.CharField(max_length=100)
+    point_price = models.IntegerField()
+    price = MoneyField(
+        max_digits=19,
+        decimal_places=4,
+        default_currency='KRW'
+    )
     barcode_number = models.CharField(max_length=100)
     expired_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
