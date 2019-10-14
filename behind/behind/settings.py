4e2da4e552855d9a1fb8ca9f2da9b37d82eb1a30
RobinRheem <robin.rheem@gmail.com>
2019-10-14 23:44:57 +0900

    Add debug to dotenv

--- behind/behind/settings.py
+++ behind/behind/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 SALT = os.getenv('SALT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
