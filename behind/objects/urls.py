2d288978de279c277aaef5d2e354a1c61ca5744f
RobinRheem <robin.rheem@gmail.com>
2019-10-14 15:09:43 +0900

    Implement object retrieve API

--- behind/objects/urls.py
+++ behind/objects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from objects.views import ObjectCreateView, ObjectAliasView
+from objects.views import ObjectCreateView, ObjectAliasView, ObjectRetrieveView
 
 urlpatterns = [
     path('', ObjectCreateView.as_view()),
     path('<name>/', ObjectAliasView.as_view()),
+    path('<component>/<type>/<id>/', ObjectRetrieveView.as_view()),
 ]
--- behind/objects/views.py
+++ behind/objects/views.py
@@ -1,11 +1,15 @@
-from django.http import Http404
+from django.contrib.contenttypes.models import ContentType
+from django.http import Http404, HttpResponseForbidden
+from django.shortcuts import redirect
+from django.views.generic.base import View
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from objects.models import Object
 from objects.serializers import CreateObjectSerializer, AliasObjectSerializer, STATE
+from purchases.models import Purchase
 
 
 class ObjectCreateView(CreateAPIView):
@@ -35,3 +39,29 @@ class ObjectAliasView(UpdateAPIView):
             return Object.objects.get(name=self.kwargs['name'], state=STATE[0][0])
         except Object.DoesNotExist:
             raise Http404()
+
+
+class ObjectRetrieveView(View):
+    def get_object(self):
+        try:
+            link_alias = f"{self.kwargs['type']}/{self.kwargs['id']}/"
+            return Object.objects.get(link_alias=link_alias, state=STATE[1][0])
+        except Object.DoesNotExist:
+            raise Http404()
+
+    def get(self, request, *args, **kwargs):
+        aliased_object = self.get_object()
+        if kwargs['type'] == 'gifticons':
+            # Check if user is gifticon owner
+            gifticon_type = ContentType.objects.get(
+                app_label='rewards',
+                model='gifticon'
+            )
+            gifticon_owner = request.user.is_authenticated and Purchase.objects.filter(
+                transaction_to=request.user,
+                item_type=gifticon_type,
+                item_id=kwargs['id']
+            ).exists()
+            if not gifticon_owner:
+                return HttpResponseForbidden()
+        return redirect(aliased_object.object.url)
