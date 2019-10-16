f31a88355af4481e878ab07a3c586867fce5bfbc
RobinRheem <robin.rheem@gmail.com>
2019-10-16 17:52:19 +0900

    Format payment model fields in admin tool

--- behind/payments/admin.py
+++ behind/payments/admin.py
@@ -5,6 +5,10 @@ from payments.models import Payment
 
 class PaymentAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = (
+        'item_name', 'user', 'price', 'pg_name',
+        'method_name', 'status', 'authority',
+    )
 
 
 admin.site.register(Payment, PaymentAdmin)
