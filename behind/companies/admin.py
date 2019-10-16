3d69f15a499ed7586d0455b22b7068d00581c5af
RobinRheem <robin.rheem@gmail.com>
2019-10-16 18:01:11 +0900

    Format companies module model fields in admin tool

--- behind/companies/admin.py
+++ behind/companies/admin.py
@@ -4,14 +4,21 @@ from companies.models import Company, Job, UserJobHistory
 
 class CompanyAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('name', 'email_domain',)
 
 
 class JobAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('title',)
 
 
 class UserJobHistoryAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = (
+        'user', 'company', 'job',
+        'confirmed', 'confirmation_method',
+        'confirmation_information',
+    )
 
 
 admin.site.register(Company, CompanyAdmin)
