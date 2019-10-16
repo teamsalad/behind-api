e34a04d3d13aa48ea4261d93d433ef7867c82a0b
RobinRheem <robin.rheem@gmail.com>
2019-10-16 15:49:58 +0900

    Format purchase model fields in admin tool

--- behind/purchases/admin.py
+++ behind/purchases/admin.py
@@ -4,6 +4,10 @@ from purchases.models import Purchase
 
 class PurchaseAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = (
+        'transaction_from', 'transaction_to',
+        'amount', 'item', 'type', 'state',
+    )
 
 
 admin.site.register(Purchase, PurchaseAdmin)
