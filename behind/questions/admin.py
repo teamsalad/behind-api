b77ca33d22f789ad61411b80bd00f1d90774f09d
RobinRheem <robin.rheem@gmail.com>
2019-10-16 15:47:41 +0900

    Format questions module model fields in admin tool

--- behind/questions/admin.py
+++ behind/questions/admin.py
@@ -5,10 +5,12 @@ from questions.models import Question, Answer
 
 class QuestionAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('content', 'questioner', 'company', 'job',)
 
 
 class AnswerAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('content', 'answerer', 'question')
 
 
 admin.site.register(Question, QuestionAdmin)
