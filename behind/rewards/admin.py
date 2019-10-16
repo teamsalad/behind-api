508b130a3b6aa4a962d112ef2f7b1fd8393ae7c2
RobinRheem <robin.rheem@gmail.com>
2019-10-16 15:45:25 +0900

    Format gifticon model fields in admin tool

--- behind/rewards/admin.py
+++ behind/rewards/admin.py
@@ -5,6 +5,7 @@ from rewards.models import Gifticon
 
 class GifticonAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('name', 'exchange', 'point_price', 'price', 'supplier',)
 
 
 admin.site.register(Gifticon, GifticonAdmin)
