1b6acd16223ff6ef87871eb2e741a3290de6d17b
RobinRheem <robin.rheem@gmail.com>
2019-10-16 17:56:54 +0900

    Format object model fields in admin tool

--- behind/objects/admin.py
+++ behind/objects/admin.py
@@ -5,6 +5,7 @@ from objects.models import Object
 
 class ObjectAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
+    list_display = ('link_alias', 'type', 'state', 'object',)
 
 
 admin.site.register(Object, ObjectAdmin)
