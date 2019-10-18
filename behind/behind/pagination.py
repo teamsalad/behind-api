6127e3e5fdf79dbf8c8bce22eff0bda6c6f8ca6f
RobinRheem <robin.rheem@gmail.com>
2019-10-18 13:54:00 +0900

    Give pagination off option

--- behind/behind/pagination.py
+++ behind/behind/pagination.py
@@ -4,3 +4,8 @@ from rest_framework.pagination import CursorPagination
 class CreatedAtCursorPagination(CursorPagination):
     page_size = 50
     ordering = '-created_at'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'all' == request.query_params.get(self.cursor_query_param):
+            return None
+        return super().paginate_queryset(queryset, request, view)
