ad16f89372edb1848764d2dd79d1a5b73793b317
RobinRheem <robin.rheem@gmail.com>
2019-10-21 20:00:42 +0900

    Configure gunicorn to use gevent

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -2,5 +2,7 @@ import multiprocessing
 
 
 bind = 'unix:/var/run/gunicorn.sock'
+worker_class = 'gevent'
+worker_connections = (multiprocessing.cpu_count() * 2 + 1) * 1000
 workers = multiprocessing.cpu_count() * 2 + 1
 
