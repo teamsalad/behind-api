effcc2ddf7414a57f1906c5e4bf17ecb25654b78
RobinRheem <robin.rheem@gmail.com>
2019-10-29 15:36:34 +0900

    Make asgi and wsgi only read production env file

--- behind/behind/asgi.py
+++ behind/behind/asgi.py
@@ -8,7 +8,7 @@ import django
 import dotenv
 from channels.routing import get_default_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env.production'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "behind.settings")
 django.setup()
 
